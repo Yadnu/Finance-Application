b61a320377bbeee5eb402537a78d3422f8f0b7fd
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-05-26 01:38:32 +0530

    add components

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({accounts}: DoughnutChartProps) => {
+  return (
+    
+    <Doughnut data={[]} />
+  )
+}
+
+export default DoughnutChart
